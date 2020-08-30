12e321b9167762a48465f33692b1b1a15387a4c5
Illia <illia2143@gmail.com>
2020-08-30 00:15:45 -0400

    accessibility improved + css grid added + some css refactured + coments added

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ moviesApp.getGenresId = () => {
 
 // Append the genres on DOM 
 moviesApp.displayGenresList = function(genresNames) {
-genresNames.forEach(function(genres) {
-    const genresAll =
-    `
-    <div class="everyGenre">
-        <input type="checkbox" id="${genres.name}" name="${genres.name}" value="${genres.id}">
-        <label for="${genres.name}">${genres.name}</label>
-    </div>
-    `
-    $('.genres').append(genresAll);
-    })
+    genresNames.forEach(function(genres) {
+        const genresAll =
+        `
+        <div class="everyGenre" aria-label="${genres.name} genre">
+            <input type="checkbox" id="${genres.name}" name="${genres.name}" value="${genres.id}">
+            <label for="${genres.name}">${genres.name}</label>
+        </div>
+        `
+        $('.genres').append(genresAll);
+        })
 }
 
 // Putting all IDs of the movie types into array for later use
@@ -37,28 +37,32 @@ moviesApp.genresID = (ids) => {
             return id.id;
         });
 }
-console.log(moviesApp.IDs);
 
 // Selecting one or multiple genres(specifying year is optional) and passing them to find movies besed on the selected genres and(or) year
 moviesApp.selectGenre = function() {
 
-    $('input[type="submit"]').on('click', function(event){
+    $('input[type="submit"]').on('click', function(event) {
         event.preventDefault();
 
         $('section').empty();
         const selected = $('.genres .everyGenre input[type="checkbox"]');
-
+        
+        // getting only the checkboxes that are CHECKED and storing them in array
         const ids = [];
         $(selected).each(function() {
-            if ($(this).prop('checked')) {             
+            if ($(this).prop('checked')) {   
                 ids.push($(this).val());
                 };
             });
 
         const year = $('input[type="text"]').val();
         
-        // checking if any genres selected and that year has valid input(1. " " = no choice for year; 2. 1873 = the first ever movie; 3. Is a number) 
-        if ((ids.length !== 0 || year == '' || year >= 1873) && (!isNaN(year))) {
+        // checking if any genres selected and that the year has a valid input
+        // 1. " " = no choice for year; 
+        // 2. 1873 = the first ever movie; 
+        // 3. Is a number);
+
+        if ( (ids.length !== 0 || year == '' || year >= 1873) && (!isNaN(year)) ) {
                 moviesApp.getMovies(ids, year);
                 moviesApp.scroll('.results');
 
@@ -67,9 +71,10 @@ moviesApp.selectGenre = function() {
                 $('.search-bar input[type="text"]').attr('placeholder', 'Enter a year (optional)'); 
                 $('.search-bar input[type="text"]').removeClass('warning');
 
-                const startOver = `
+                const startOver = 
+                `
                 <button type="button" class="startOver">Restart</button>
-                `; 
+                `;
             
                 $('.restart').append(startOver);
 
@@ -101,8 +106,7 @@ moviesApp.getMovies = function(type, year) {
 
 //appending movie names on DOM
 moviesApp.displayNames = function(name) {
-
-    name.forEach(function(movieName) {
+    name.forEach( function(movieName) {
         moviesApp.foundResults(movieName);
         $('section').append(foundResults);
     });
@@ -122,9 +126,10 @@ moviesApp.foundResults = (movie) => {
         <p class="rating">Rating: ${movie.vote_average} out of 10</p>
     
         <details class="description">
-        <summary><i class="fas fa-plus"></i> Description
-        </summary>
-        <p>${movie.overview}</p>
+            <summary>
+                    <i class="fas fa-plus" aria-hidden="true" ></i> Description
+            </summary>
+            <p>${movie.overview}</p>
         </details>
 
         <button type="button">Add to watched</button>
@@ -137,15 +142,20 @@ moviesApp.startOver = function() {
     $('.restart').on('click', function(event){
         event.preventDefault();
         
+        // clearing input field
         $('input[type="text"]').val('');
-        if ($('.genres .everyGenre input[type="checkbox"]').prop("checked", true)) 
-        {
+
+        // unchecking checked checkboxes
+        if ($('.genres .everyGenre input[type="checkbox"]').prop("checked", true)) {
             $('.genres .everyGenre input[type="checkbox"]').prop("checked", false)
         }
 
+        // removing restart button (it's only to be shown when we have results)
         $('.restart').empty();
+        // going to the top of the page
         moviesApp.scroll('h1');
 
+        // making sure the results disappear with delay(not instantly)
         setTimeout(() => {
             $('section').empty();
         }, 1300);
@@ -162,15 +172,23 @@ moviesApp.showWatched = () => {
 
 let prevSibl = '';
 
-//adding movie to seen
+//adding a movie to seen
 moviesApp.addToWatched = () => {
 
     $('.results').on('click', 'button[type="button"]', function(event){
         event.preventDefault();
 
         prevSibl = $(this).parent();
+        console.log(this);
+        console.log(prevSibl);
+
+        // appending the saved movie on the side bar
         $(prevSibl).clone().prependTo("div.watched-movies");
+
+        // removing the button "Add to watched" as it's already gonna be there
         $('div.watched-movies .movie-container button').remove();
+
+        // filling the space with a random movie
         moviesApp.getRandomMovie();
     });
 }
@@ -183,13 +201,17 @@ moviesApp.getRandomMovie = function() {
         method: 'GET',
         data: {
             api_key: `4b0a04de3275456e960df5811ac4bafd`,
-            page: `${Math.floor(Math.random()*500)}`,
+            page: `${Math.floor(Math.random()*500)}`, // getting random PAGE from API. One out of 500
         }
     }).then(function(receivedResults) {
-        // save API data
+        // Each PAGE has 20 movies. Below, getting a random movie out of 20
         moviesApp.data = receivedResults.results[Math.floor(Math.random()*receivedResults.results.length)];
         let movieName = moviesApp.data;
+
+        // passing a new movie to make it appear
         moviesApp.foundResults(movieName);
+
+        // replacing a previous movie(that went to SEEN section with a new RANDOM movie)
         $(prevSibl).replaceWith(foundResults);        
     });
 }
@@ -216,11 +238,6 @@ $(function() {
     moviesApp.init();
 })
 
-
-// Questions(further work)
+// Further work
 // 1. I wanted to populate an empty spot of the movie that went to "seen" section NOT with a random movie, BUT with the one
 // with the genre(s) and year specified. So, sending every movie to "seen" section, I wanted to populate new empty spots with new movies one by one from 1 to 20 taking them from the NEXT page (2). When the last movie(2) form page 2 got moved, then use page 3 and so on. API is structured in a way so that there's only 20 movies max per page. Also, while getting data, it seems that milpiple pages can't be specified. Could you suggest the best way on how to do that?
-
-// 2. Checkboxes turn out to be unfocusable, which isn't good for accesability. I wonder what could be the best alternative/solution to this problem?
-
-// Thank you!
